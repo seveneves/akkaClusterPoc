336dc1c962b3c4a0ee7fcde424fb20bf3f791d80
Vanya Stanislavciuc <istanislavciuc@xebialabs.com>
2015-09-12 14:43:14 +0200

    Using akka 2.4.0-RC2

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += "com.typesafe.akka" %% "akka-cluster" % "2.3.13"
-libraryDependencies += "com.typesafe.akka" %% "akka-contrib" % "2.3.13"
-libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.1.3"
+val akkaVersion = "2.4.0-RC2"
+libraryDependencies += "com.typesafe.akka" %% "akka-cluster" % akkaVersion
+libraryDependencies += "com.typesafe.akka" %% "akka-cluster-sharding" % akkaVersion
+libraryDependencies += "org.iq80.leveldb"            % "leveldb"          % "0.7"
+libraryDependencies += "org.fusesource.leveldbjni"   % "leveldbjni-all"   % "1.8"
--- src/main/scala-2.11/com/xebialbas/shool/cluster/PingSupervisor.scala
+++ src/main/scala-2.11/com/xebialbas/shool/cluster/PingSupervisor.scala
@@ -1,7 +1,7 @@
 package com.xebialbas.shool.cluster
 
 import akka.actor.{Actor, Props}
-import akka.contrib.pattern.{ClusterSharding, ShardRegion}
+import akka.cluster.sharding.{ClusterShardingSettings, ClusterSharding, ShardRegion}
 import com.xebialbas.shool.cluster.PingSupervisor.PingMessage
 
 object PingSupervisor {
@@ -16,19 +16,20 @@ object PingSupervisor {
 
 class PingSupervisor extends Actor {
 
-  val extractor: ShardRegion.IdExtractor = {
+  val extractor: ShardRegion.ExtractEntityId = {
     case PingMessage(id, msg) => id -> msg
   }
 
-  val shardResolver: ShardRegion.ShardResolver = {
+  val shardResolver: ShardRegion.ExtractShardId = {
     case PingMessage(id, msg) => id(0).toString
   }
 
   val pingRegion = ClusterSharding(context.system).start(
     typeName = "ping",
-    entryProps = Option(PingActor.props),
-    idExtractor = extractor,
-    shardResolver = shardResolver
+    entityProps = PingActor.props,
+    settings = ClusterShardingSettings(context.system),
+    extractEntityId = extractor,
+    extractShardId = shardResolver
   )
 
   override def receive: Receive = {
